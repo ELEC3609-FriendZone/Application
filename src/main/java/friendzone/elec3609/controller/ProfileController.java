27a87ab3794daf0f4a31beefc86673a880f446dc
marjorie4493 <marjorie4493@hotmail.com>
2013-10-20 18:02:48 +1100

    Profile page content with dummy data

--- src/main/java/friendzone/elec3609/controller/ProfileController.java
+++ src/main/java/friendzone/elec3609/controller/ProfileController.java
@@ -1,5 +1,7 @@
 package friendzone.elec3609.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 
@@ -8,6 +10,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import friendzone.elec3609.service.DatabaseHandler;
+import friendzone.elec3609.model.Role;
+import friendzone.elec3609.model.Student;
+import friendzone.elec3609.model.StudyLevel;
+import friendzone.elec3609.model.ProgrammingLanguage;
 
 @Controller
 public class ProfileController {
@@ -15,9 +21,40 @@ public class ProfileController {
 	@Autowired
 	DatabaseHandler dbHandler;
 	
+	//New Dummy data just for testing
+	Student student = new Student("123456789", "abcd1234", "aFirstName", "aLastname", "abcd1234@uni.sydney.edu.au", "0412345678", StudyLevel.UNDERGRADUATE, true);
+	
+
+	
 	@RequestMapping("/profile")
 	public String getEnums(Map<String, Object> map){
-
+		//More Dummy Data
+		ProgrammingLanguage[] planguages = new ProgrammingLanguage[]{ProgrammingLanguage.C, ProgrammingLanguage.CPLUSPLUS};
+		student.setLanguages( planguages );
+		student.setPreferredRole(Role.PROGRAMMER);
+		
+		//Converts the array of Prorgramming Language array to a 
+		//List of strings
+		ProgrammingLanguage[] languages = student.getLanguages();
+		List<String> langStrings = new ArrayList<String>();
+		for (ProgrammingLanguage language : languages){
+			langStrings.add(language.toString());
+		}
+		
+		
+		//Data to be put on the profile page
+		map.put("SID", student.getSID());
+		map.put("unikey", student.getUnikey());
+		map.put("firstName", student.getFirstName());
+		map.put("lastName", student.getLastName());
+		map.put("primaryEmail", student.getPrimaryEmail());
+		map.put("secondaryEmail", student.getSecondaryEmail());
+		map.put("mobile", student.getMobile());
+		map.put("studyLevels", student.getStudyLevel());
+		map.put("ESL", student.getESL());
+		map.put("languages", langStrings);
+		map.put("preferredRole", student.getPreferredRole());
+		
 		return "profile";
 	}
 	
--- src/main/webapp/WEB-INF/jsp/profile.jsp
+++ src/main/webapp/WEB-INF/jsp/profile.jsp
@@ -1,3 +1,5 @@
+<%@taglib uri="http://www.springframework.org/tags" prefix="spring"%>
+<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <html>
 	<head>
 		<title>Friendzone-Profile</title>
@@ -28,22 +30,82 @@
 	
 			.page-body {
 				background: rgba(260,260, 260, 1);
-				margin-top:1%;
-				margin-left:10%;
+				margin-left:2%;
 				margin-right:10%;
 			}
-			
-			.formTable{
-			
+			.profile-details {
+				background: rgba(260,260, 260, 1);
+				margin-left:10%;
+				margin-right:2%;
 			}
+			.user-details{
+				margin-top:1%;
+				margin-left:10%;
+			}
+			table{
+				width:100%;
+			}
+			.links{
+				text-align:right;
+				margin-right:10%;
+			}
+			.details{
+				vertical-align:top;
+				margin-right:10%;
+				width: 20%;
+			}
+
 		</style>
 	</head>
 	<body>
 		<div class="page-header">
 			<a href="/mainHome/"class="appTitle"><h1>FriendZone</h1></a>
 		</div>
-		<div class="page-body">
-			<h1>Profile Page</h1>
-		</div>
+		<table>
+			<tr>
+				<td>
+					<div class="user-details">
+						<h2>Profile</h2>
+						<i><h3>${firstName} ${lastName}</h3></i>
+					</div>
+				</td>
+				<td></td>
+			</tr>
+			<tr>
+				<td class="details">
+					<div class="links">
+						<a href="/profile/">edit</a>
+					</div>			
+					<div class="profile-details">
+						<h3>Details</h3>
+						${primaryEmail} <br>
+						${secondaryEmail}<br>
+						${mobile}
+					</div>
+				</td>
+				<td>
+					<div class="links">
+						<a href="/profile/">edit</a>
+					</div>		
+					<div class="page-body">
+						<h2>Preferences</h2>
+						<h3>Study Levels</h3>
+						<ul>
+							${studyLevels}
+						</ul>
+						<h3>Role</h3>
+						<ul>
+							${preferredRole}
+						</ul>
+						<h3>Languages</h3>
+						<ul>
+							<c:forEach items="${languages}" var="language">
+								<li>${language}</li>
+							</c:forEach>
+						</ul>
+					</div>
+				</td>
+			</tr>
+		</table>
 	</body>
 </html>
