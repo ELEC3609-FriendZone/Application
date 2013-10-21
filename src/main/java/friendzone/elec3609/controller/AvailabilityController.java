a0618fb74153e56c30b4424741b27f9e8061332c
marjorie4493 <marjorie4493@hotmail.com>
2013-10-21 14:22:58 +1100

    Availability controller and view pages made to show information

--- src/main/java/friendzone/elec3609/controller/AvailabilityController.java
+++ src/main/java/friendzone/elec3609/controller/AvailabilityController.java
@@ -1,5 +1,7 @@
 package friendzone.elec3609.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 
@@ -7,6 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import friendzone.elec3609.model.ProgrammingLanguage;
+import friendzone.elec3609.model.Project;
+import friendzone.elec3609.model.Student;
+import friendzone.elec3609.model.StudyLevel;
+import friendzone.elec3609.model.Team;
+import friendzone.elec3609.model.UnitOfStudy;
 import friendzone.elec3609.service.DatabaseHandler;
 
 @Controller
@@ -15,9 +23,69 @@ public class AvailabilityController {
 	@Autowired
 	DatabaseHandler dbHandler;
 	
+	//Dummy Data For now, so i can test the content on the page
+	Student student = new Student("123456789", "abcd1234", "aFirstName", "aLastname", "abcd1234@uni.sydney.edu.au", "0412345678", StudyLevel.UNDERGRADUATE, true);
+	UnitOfStudy uos =  new UnitOfStudy("ELEC3609", "Internet Software Platforms", 500);
+	Project project = new Project(uos,  new java.sql.Date(8099,10,15));
+	Team team = new Team(12345, project);
+	
+	//Dummy boolean availability data
+	boolean[][] availability = new boolean[][]{{true, true, false, false, true, false, true, false, false, false, true, false},
+												{true, true, false, false, true, true, false, true, true, false, true, false},
+												{true, true, false, false, false, false, true, true, false, false, true, false},
+												{false, true, false, true, true, false, true, true, false, false, true, false},
+												{true, false, false, true, false, false, false, true, true, false, true, false},
+												{false, true, false, false, true, false, true, false, false, false, true, false},
+												{true, true, false, true, false, false, true, true, false, false, true, false}};
+										
+
+	
 	@RequestMapping("/availability")
 	public String getEnums(Map<String, Object> map){
+		//storing the data of availability
+		//availability information
+		student.setAvailability(availability);
 
+		
+		List<String> availMondayStrings = new ArrayList<String>();
+		List<String> availTuesdayStrings = new ArrayList<String>();
+		List<String> availWednesdayStrings = new ArrayList<String>();
+		List<String> availThursdayStrings = new ArrayList<String>();
+		List<String> availFridayStrings = new ArrayList<String>();
+		List<String> availSaturdayStrings = new ArrayList<String>();
+		List<String> availSundayStrings = new ArrayList<String>();
+		
+		for(int j=0;j<12; j++)
+		{
+			availMondayStrings.add(String.valueOf(student.getAvailability()[0][j]));
+			availTuesdayStrings.add(String.valueOf(student.getAvailability()[1][j]));
+			availWednesdayStrings.add(String.valueOf(student.getAvailability()[2][j]));
+			availThursdayStrings.add(String.valueOf(student.getAvailability()[3][j]));
+			availFridayStrings.add(String.valueOf(student.getAvailability()[4][j]));
+			availSaturdayStrings.add(String.valueOf(student.getAvailability()[5][j]));
+			availSundayStrings.add(String.valueOf(student.getAvailability()[6][j]));
+		}
+			
+
+
+		//The heading of the page will include the Unit Of Study Details
+		map.put("unitcode", uos.getUnitCode());
+		map.put("unitname", uos.getUnitName());
+		
+		//The side bar of the page, includes team member details
+		map.put("team", team.getMembers());
+		
+		//The availability Stuff for the body information
+		//map.put("availability", student.getAvailability());
+		map.put("availabilityMonday", availMondayStrings);
+		map.put("availabilityTuesday", availTuesdayStrings);
+		map.put("availabilityWednesday", availWednesdayStrings);
+		map.put("availabilityThursday", availThursdayStrings);
+		map.put("availabilityFriday", availFridayStrings);
+		map.put("availabilitySaturday", availSaturdayStrings);
+		map.put("availabilitySunday", availSundayStrings);
+		
+		//return page
 		return "availability";
 	}
 	
--- src/main/webapp/WEB-INF/jsp/availability.jsp
+++ src/main/webapp/WEB-INF/jsp/availability.jsp
@@ -1,5 +1,7 @@
+<%@taglib uri="http://www.springframework.org/tags" prefix="spring"%>
+<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <html>
-	<head>
+<head>
 		<title>Friendzone-Availability</title>
 		<style type="text/css">
 			body {
@@ -29,21 +31,156 @@
 			.page-body {
 				background: rgba(260,260, 260, 1);
 				margin-top:1%;
-				margin-left:10%;
+				margin-left:2%;
 				margin-right:10%;
 			}
-			
-			.formTable{
-			
+			.team-member-details {
+				background: rgba(260,260, 260, 1);
+				margin-top:1%;
+				margin-left:10%;
+				margin-right:2%;
 			}
+			.other-page-links {
+				text-align: right;
+				margin-right:10%;
+			}
+			table{
+				width:100%;
+			}
+			.subject-details {
+				margin-top:1%;
+				margin-left:10%;
+			}
+			.table-data-team{
+				vertical-align:top;
+				width:20%;
+			}
+
 		</style>
 	</head>
 	<body>
 		<div class="page-header">
 			<a href="/mainHome/"class="appTitle"><h1>FriendZone</h1></a>
 		</div>
-		<div class="page-body">
-			<h1>Availability Page</h1>
-		</div>
+		<table>
+			<tr>
+				<td>
+					<div class="subject-details">
+						<h2>${unitcode}</h2>
+						<i><h3>${unitname}</h3></i>
+					</div>
+				</td>
+				<td></td>
+			</tr>
+			<tr>
+				<td class="table-data-team">		
+					<div class="team-member-details">
+						<h3>Team members</h3>
+						${team}
+					</div>
+				</td>
+				<td>
+					<div class="other-page-links">
+						<a href="/subject/">Details</a> |
+						<a href="/meeting/">New Meeting</a> |
+						<a href="/meeting/">View Meeting</a> |
+						<a href="/availability/">My Availability</a> |
+						<a href="/subject/">Chat</a>
+					</div>		
+					<div class="page-body">
+						<h3>Availability</h3>
+						
+						<table>
+							<tr>
+								<td>Day</td>
+								<td>9am</td>
+								<td>10am</td>
+								<td>11am</td>
+								<td>12pm</td>
+								<td>1pm</td>
+								<td>2pm</td>
+								<td>3pm</td>
+								<td>4pm</td>
+								<td>5pm</td>
+								<td>6pm</td>
+								<td>7pm</td>
+								<td>8pm</td>
+							</tr>
+							<tr>
+								<td>Monday</td>
+								<c:forEach items="${availabilityMonday}" var="aMonday">
+									<td>
+										<c:if test="${aMonday=='true'}">
+										 X
+										</c:if>
+									</td>
+									
+								</c:forEach>
+							</tr>
+							<tr>
+								<td>Tuesday</td>
+								<c:forEach items="${availabilityTuesday}" var="aTuesday">
+									<td>
+										<c:if test="${aTuesday=='true'}">
+										 X
+										</c:if>
+									</td>
+								</c:forEach>
+							</tr>
+							<tr>
+								<td>Wednesday</td>
+								<c:forEach items="${availabilityWednesday}" var="aWednesday">
+									<td>
+										<c:if test="${aWednesday=='true'}">
+										 X
+										</c:if>
+									</td>
+								</c:forEach>
+							</tr>
+							<tr>
+								<td>Thursday</td>
+								<c:forEach items="${availabilityThursday}" var="aThursday">
+									<td>
+										<c:if test="${aThursday=='true'}">
+										 X
+										</c:if>
+									</td>
+								</c:forEach>
+							</tr>
+							<tr>
+								<td>Friday</td>
+								<c:forEach items="${availabilityFriday}" var="aFriday">
+									<td>
+										<c:if test="${aFriday=='true'}">
+										 X
+										</c:if>
+									</td>
+								</c:forEach>
+							</tr>
+							<tr>
+								<td>Saturday</td>
+								<c:forEach items="${availabilitySaturday}" var="aSaturday">
+									<td>
+										<c:if test="${aSaturday=='true'}">
+										 X
+										</c:if>
+									</td>
+								</c:forEach>
+							</tr>
+							<tr>
+								<td>Sunday</td>
+								<c:forEach items="${availabilitySunday}" var="aSunday">
+									<td>
+										<c:if test="${aSunday=='true'}">
+										 X
+										</c:if>
+									</td>
+								</c:forEach>
+							</tr>
+						</table>
+					</div>
+				</td>
+			</tr>
+		</table>
 	</body>
 </html>
